aaa5f1e890521b836cc03fa58811897e5e947a8b
Zach <ss3808924@gmail.com>
2023-01-09 10:00:19 +0800

    refined

--- main/main.go
+++ main/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/TsubasaEX/SampleGoModPkg/greeting"
+	mygreeting "github.com/TsubasaEX/SampleGoModPkg/greeting"
 	myshow "github.com/TsubasaEX/SampleGoModPkg/testshow/show"
 	"github.com/TsubasaEX/SampleGoModTest/greeting"
 )
 
 func main() {
 	greeting.Say("Hi World")
-	greeting.SayWithColor("Hi World")
+	greeting.SayWithColor("Hi World with Color")
 	mygreeting.SayWithHello("Zach")
 	myshow.ShowWithHello("123")
 	// f.Println("123")
